ad3b7e2fabb3299ae62c364bc6848ea9bc9dae10
Travis Truax <32211878+travis-truax@users.noreply.github.com>
2022-07-07 21:39:08 -0700

    set dilbert to use test space

--- dilbert_comic.py
+++ dilbert_comic.py
@@ -1,12 +1,13 @@
 import requests
 import data_config
 import logging
+import boto3
 
 logger = logging.getLogger()
 
 DILBERT_URL = "http://dilbert-api.glitch.me/json"
 SECRET_ID = "nike_slack_webhook_urls"
-SLACK_CHANNEL = "daily_smile"
+SLACK_CHANNEL = "test_space"
 TABLE_NAME = "nike_daily_smile_tracking"
 TABLE = boto3.resource('dynamodb').Table(TABLE_NAME)
 AWS_REGION = "us-west-2"
@@ -20,10 +21,10 @@ def get_slack_webhook_url():
         service_name='secretsmanager',
         region_name=AWS_REGION
     )
-    get_secret_value_response = client.get_secret_value(
+    secretsmanager_response = client.get_secret_value(
         SecretId=SECRET_ID
     )
-    return get_secret_value_response[SLACK_CHANNEL]
+    return secretsmanager_response[SLACK_CHANNEL]
 
 
 def get_last_comic_metadata():
@@ -42,7 +43,7 @@ def save_comic_metadata(output):
 
 dilbert_response = requests.get(url = dilbert_url)
 data = dilbert_response.json()
-output = "Title: " + str(data['title']) + "\nhttps:" + data['image']
+output = f"Title: {str(data['title'])}\nhttps:{data['image']}
 logger.info(f"Output: {output}")
 
 if get_last_comic_metadata() == output:
