3c1f209572f04d6a855df79348ec4b189ec10d24
Travis Truax <32211878+travis-truax@users.noreply.github.com>
2022-07-07 21:33:14 -0700

    resturcture dilbert to use dynamodb and secretsmanager

--- dilbert_comic.py
+++ dilbert_comic.py
@@ -1,22 +1,53 @@
 import requests
 import data_config
+import logging
 
-dilbert_r = requests.get(url = data_config.dilbert_url)
-data = dilbert_r.json()
+logger = logging.getLogger()
+
+DILBERT_URL = "http://dilbert-api.glitch.me/json"
+SECRET_ID = "nike_slack_webhook_urls"
+SLACK_CHANNEL = "daily_smile"
+TABLE_NAME = "nike_daily_smile_tracking"
+TABLE = boto3.resource('dynamodb').Table(TABLE_NAME)
+AWS_REGION = "us-west-2"
+
+# get slack webhook from secretsmanager
+# Create a Secrets Manager client
+def get_slack_webhook_url():
+    logger.info("retrieving webhook from secretsmanager")
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=AWS_REGION
+    )
+    get_secret_value_response = client.get_secret_value(
+        SecretId=SECRET_ID
+    )
+    return get_secret_value_response[SLACK_CHANNEL]
+
+
+def get_last_comic_metadata():
+    db_entry_key = {"comic_name": "dilbert"}
+    db_item = TABLE.get_item(Key=db_entry_key)
+    return db_item["output"]
+
+
+def save_comic_metadata(output):
+    db_entry = {
+        "comic_name": "dilbert",
+        "output": output
+    }
+    TABLE.put_item(Item=db_entry)
+
+
+dilbert_response = requests.get(url = dilbert_url)
+data = dilbert_response.json()
 output = "Title: " + str(data['title']) + "\nhttps:" + data['image']
-print(output)
+logger.info(f"Output: {output}")
 
-memory = open('/home/ubuntu/PetProjects/Slack_Daily_Joke/dilbert_last.txt', 'r')
-last_comic = memory.read()
-memory.close()
-
-push = { "text": output }
-
-daily_headers = { "content-type": "application/json" }
-
-if last_comic != output:
-    slack_r = requests.post(url = data_config.daily_url, data = str(push), headers = daily_headers)
-    memory = open('/home/ubuntu/PetProjects/Slack_Daily_Joke/dilbert_last.txt', 'w')
-    memory.write(output)
-    memory.close()
-    
+if get_last_comic_metadata() == output:
+    # send to slack
+    daily_headers = { "content-type": "application/json" }
+    slack_response = requests.post(url=get_slack_webhook_url(), data=str({"text": output}), headers=daily_headers,)
+    # save metadata
+    save_comic_metadata(output)
