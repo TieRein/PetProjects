52faa59869f306383d4989b143fed9f76223289d
Travis Truax <32211878+travis-truax@users.noreply.github.com>
2022-07-07 22:20:30 -0700

    align xkcd with dibert to make it verky

--- xkcd_comic.py
+++ xkcd_comic.py
@@ -1,8 +1,8 @@
-import requests
-import data_config
+from botocore.vendored import requests
+import logging
 import boto3
+import json
 
-logger = logging.getLogger()
 
 XKCD_URL = "https://xkcd.com/info.0.json"
 SECRET_ID = "nike_slack_webhook_urls"
@@ -10,6 +10,7 @@ SLACK_CHANNEL = "test_space"
 TABLE_NAME = "nike_daily_smile_tracking"
 TABLE = boto3.resource('dynamodb').Table(TABLE_NAME)
 AWS_REGION = "us-west-2"
+logger = logging.getLogger()
 
 # get slack webhook from secretsmanager
 # Create a Secrets Manager client
@@ -20,16 +21,17 @@ def get_slack_webhook_url():
         service_name='secretsmanager',
         region_name=AWS_REGION
     )
-    get_secret_value_response = client.get_secret_value(
+    secretsmanager_response = client.get_secret_value(
         SecretId=SECRET_ID
     )
-    return get_secret_value_response[SLACK_CHANNEL]
+    secret_string = json.loads(secretsmanager_response["SecretString"])
+    return secret_string[SLACK_CHANNEL]
 
 
 def get_last_comic_metadata():
     db_entry_key = {"comic_name": "xkcd"}
     db_item = TABLE.get_item(Key=db_entry_key)
-    return db_item["output"]
+    return db_item.get("output")
 
 
 def save_comic_metadata(output):
@@ -38,16 +40,19 @@ def save_comic_metadata(output):
         "output": output
     }
     TABLE.put_item(Item=db_entry)
-
-
-xkcd_response = requests.get(url=XKCD_URL)
-data = xkcd_response.json()
-output = "Comic Number: " + str(data['num']) + "\nTitle: " + data['title'] + "\nAlt Text: " + data['alt'] + "\n" + data['img']
-logger.info(f"Output: {output}")
-
-if get_last_comic_metadata() == output:
-    # send to slack
-    daily_headers = { "content-type": "application/json" }
-    slack_response = requests.post(url=get_slack_webhook_url(), data=str({"text": output}), headers=daily_headers,)
-    # save metadata
-    save_comic_metadata(output)
+    
+    
+def lambda_handler(event, context):
+    xkcd_response = requests.get(url=XKCD_URL)
+    data = xkcd_response.json()
+    output = "Comic Number: " + str(data['num']) + "\nTitle: " + data['title'] + "\nAlt Text: " + data['alt'] + "\n" + data['img']
+    logger.info(f"Output: {output}")
+    
+    if get_last_comic_metadata() != output:
+        # send to slack
+        daily_headers = { "content-type": "application/json" }
+        slack_response = requests.post(url=get_slack_webhook_url(), data=str({"text": output}), headers=daily_headers)
+        # save metadata
+        save_comic_metadata(output)
+        return slack_response   
+    return "no new comic"
