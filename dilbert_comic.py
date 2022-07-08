9ff0d2d4656cd7ddbc82279782783c61d25bdf49
Travis Truax <32211878+travis-truax@users.noreply.github.com>
2022-07-07 22:16:27 -0700

    its verking. dilbert publishes successfully

--- dilbert_comic.py
+++ dilbert_comic.py
@@ -1,9 +1,8 @@
-import requests
-import data_config
+from botocore.vendored import requests
 import logging
 import boto3
+import json
 
-logger = logging.getLogger()
 
 DILBERT_URL = "http://dilbert-api.glitch.me/json"
 SECRET_ID = "nike_slack_webhook_urls"
@@ -11,6 +10,7 @@ SLACK_CHANNEL = "test_space"
 TABLE_NAME = "nike_daily_smile_tracking"
 TABLE = boto3.resource('dynamodb').Table(TABLE_NAME)
 AWS_REGION = "us-west-2"
+logger = logging.getLogger()
 
 # get slack webhook from secretsmanager
 # Create a Secrets Manager client
@@ -24,13 +24,15 @@ def get_slack_webhook_url():
     secretsmanager_response = client.get_secret_value(
         SecretId=SECRET_ID
     )
-    return secretsmanager_response[SLACK_CHANNEL]
+    print(secretsmanager_response)
+    secret_string = json.loads(secretsmanager_response["SecretString"])
+    return secret_string[SLACK_CHANNEL]
 
 
 def get_last_comic_metadata():
     db_entry_key = {"comic_name": "dilbert"}
     db_item = TABLE.get_item(Key=db_entry_key)
-    return db_item["output"]
+    return db_item.get("output")
 
 
 def save_comic_metadata(output):
@@ -39,16 +41,18 @@ def save_comic_metadata(output):
         "output": output
     }
     TABLE.put_item(Item=db_entry)
+    
+    
+def lambda_handler(event, context):
+    dilbert_response = requests.get(url=DILBERT_URL)
+    data = dilbert_response.json()
+    output = f"Title: {str(data['title'])}\nhttps:{data['image']}"
+    logger.info(f"Output: {output}")
+    
+    if get_last_comic_metadata() != output:
+        # send to slack
+        daily_headers = { "content-type": "application/json" }
+        slack_response = requests.post(url=get_slack_webhook_url(), data=str({"text": output}), headers=daily_headers)
+        # save metadata
+        save_comic_metadata(output)
 
-
-dilbert_response = requests.get(url = dilbert_url)
-data = dilbert_response.json()
-output = f"Title: {str(data['title'])}\nhttps:{data['image']}
-logger.info(f"Output: {output}")
-
-if get_last_comic_metadata() == output:
-    # send to slack
-    daily_headers = { "content-type": "application/json" }
-    slack_response = requests.post(url=get_slack_webhook_url(), data=str({"text": output}), headers=daily_headers,)
-    # save metadata
-    save_comic_metadata(output)
